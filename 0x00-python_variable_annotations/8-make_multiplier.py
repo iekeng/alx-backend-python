cbec715e7b733ebd64985bbc30529548d67fd038
root <root@DESKTOP-QS0QAE8>
2023-08-04 09:10:57 +0100

    err fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 '''makes multiplier effect'''
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''returns a callable object'''
     return lambda x: multiplier * multiplier
