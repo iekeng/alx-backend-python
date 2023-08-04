568dbddc4c7d5d9449c96292f4fbc5e6877a373f
root <root@DESKTOP-QS0QAE8>
2023-08-04 08:41:42 +0100

    err fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''returns the square of v and the string k'''
     return (k, v**2)
