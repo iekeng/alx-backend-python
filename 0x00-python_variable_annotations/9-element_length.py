9cffd5b3e4d48da3d1560538be5aede6657dbc1d
root <root@DESKTOP-QS0QAE8>
2023-08-04 10:02:37 +0100

    returns length of a sequence

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,6 @@
 from typing import Iterable, List, Tuple, Sequence
 
 
-def element_length(lst: Iterable[int]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''returns a list of lists'''
     return [(i, len(i)) for i in lst]
