5084c50249392a8e35c84b16e01ad4aede0d0d91
root <root@DESKTOP-QS0QAE8>
2023-08-04 07:48:14 +0100

    code

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+"""ALX SE"""
 from typing import List
-'''  returns a sum of floats '''
 
 
 def sum_list(input_list: List[float]) -> float:
-    ''' loops through the input_list and adds items in the sum variable '''
-    return float(sum(input_list))
+    """Return sum of floats in a list"""
+    return sum(x for x in input_list)
