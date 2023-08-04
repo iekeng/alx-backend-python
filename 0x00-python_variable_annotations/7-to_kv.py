603f23b9126bf48a19e4e0888480071e25b99d4c
root <root@DESKTOP-QS0QAE8>
2023-08-04 08:31:13 +0100

    mixed tuple annotation

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''processes a mixed tuple'''
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    '''returns the square of v and the string k'''
+    return (k, v**2)
