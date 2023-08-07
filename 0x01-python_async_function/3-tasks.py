bb9606b14d54edb6f7df4f830d004dafdaf3aab8
root <root@DESKTOP-QS0QAE8>
2023-08-07 15:05:13 +0100

    returns async.Task object

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''creates an async task'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    '''returns task'''
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
