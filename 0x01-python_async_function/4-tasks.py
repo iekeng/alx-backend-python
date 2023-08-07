fa85dc627237b533b1cda2f0f1f97d78d3e6ff0f
root <root@DESKTOP-QS0QAE8>
2023-08-07 15:23:19 +0100

    replicates wait_n

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''imports wait_random method'''
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''Returns an array of floats'''
+    res = [await task_wait_random(max_delay) for i in range(0, n)]
+    return sorted(res)
