209d9a950945debd9a599c63665924e3de7d44c1
root <root@DESKTOP-QS0QAE8>
2023-08-07 14:23:16 +0100

    executes coroutines cocurrently

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,5 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     '''Returns an array of floats'''
-    delays = [await wait_random(max_delay) for i in range(0, n)]
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(0, n)))
     return delays
