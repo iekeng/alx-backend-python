25f84c0a6e4a25e4644ea73f9b4218087c2b7bb3
root <root@DESKTOP-QS0QAE8>
2023-08-07 15:27:20 +0100

    replicates wait_n

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''imports wait_random method'''
+'''imports task_wait_random method'''
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
