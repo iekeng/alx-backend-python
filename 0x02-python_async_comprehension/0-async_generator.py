7a17d2dcea2181741f528c90ae970605f4713513
root <root@DESKTOP-QS0QAE8>
2023-08-08 20:27:21 +0100

    anotate variables

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''async function generates yields random numbers'''
+from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     '''yields values between 0 and 10'''
     x: int = 10
     for i in range(x):
