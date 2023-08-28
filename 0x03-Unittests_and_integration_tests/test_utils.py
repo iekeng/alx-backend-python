4fbce1df345706cc81b8c61ab6dbdc9edb1fe451
root <root@DESKTOP-QS0QAE8>
2023-08-28 14:24:36 +0100

    initial commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''test_utils'''
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''Tests access_nested_map functionality'''
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        '''Employs the assertEqual method from unittest'''
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
